1b4d71108e75e68f455b31b764f42c6eebccf377
Tman99877 <wallacetyrese8@gmail.com>
2022-11-23 17:27:12 +0000

    up6

--- src/welcome-app/main.go
+++ src/welcome-app/main.go
@@ -18,17 +18,30 @@ type Welcome struct {
 type JsonResponse struct{
 	Value1 string `json:"key1"`
 	Value2 string `json:"key2"`
+	jsonNested JsonNested `json: "JsonNested`
 }
 
+type JsonNested struct{
+	NestedValue1 string `json"nestedKey1"`
+	NestedValue2 string `json"nestedKey2"`
+}
 
 
 func main() {
 
 	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
 	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+
+	nested := JsonNested{
+		NestedValue1: "first nested value",
+		NestedValue2: "Second nested value",
+	}
+
+
 	jsonResp  := JsonResponse{
 		Value1:  "Some Data",
 		Value2: "Other Data",
+		jsonNested: nested,
 
 	}
 
