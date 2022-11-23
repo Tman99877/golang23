15f3d131d403005bbe0a62b200bbc024fac7face
Tman99877 <wallacetyrese8@gmail.com>
2022-11-23 15:49:31 +0000

    up5

--- src/welcome-app/main.go
+++ src/welcome-app/main.go
@@ -36,26 +36,27 @@ func main() {
 	http.Handle("/static/", 
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("static")))) 
-	
-	
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request)) 
 
-	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		if name := r.FormValue("name"); name != "" {
-			welcome.Name = name
+			welcome.Name=name
 		}
-		
-		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+		if err := templates.ExecuteTemplate(w, "welcome-template.html",welcome); err != nil{
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-	}
-
-
-	 
-	http.HandleFunc("/jsonResponse", func(w http.ResponseWriter, r *http.Request)){
+	})
+	http.HandleFunc("/jsonResponse", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(jsonResp)
-	}
-
+	})
+	
 	fmt.Println("Listening")
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
+
+
+
+		
+
+
+	
+
